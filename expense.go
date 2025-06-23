308d9bf0cb943d656812e3d0306ba131e47e9a12
Skues <alfieskues2006@gmail.com>
2025-06-23 15:50:42 +0100

    Added saving and loading into csv

--- expense.go
+++ expense.go
@@ -1,30 +1,33 @@
 package expense
 
 import (
+	"encoding/csv"
 	"errors"
 	"fmt"
+	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
-type expense struct {
-	description string
-	amount      float64
-	createdAt   time.Time
+type Expense struct {
+	Description string
+	Amount      float64
+	CreatedAt   time.Time
 }
 
-type Expenses []expense
+type Expenses []Expense
 
-func (e *Expenses) AddExpense(description string, amount float64) {
-	var ex expense = expense{
-		description,
-		amount,
+func (e *Expenses) AddExpense(Description string, Amount float64) {
+	var ex Expense = Expense{
+		Description,
+		Amount,
 		time.Now()}
 
 	*e = append(*e, ex)
 }
 
-func (e *Expenses) UpdateExpense(id int, change string, value ...string) error {
+func (e *Expenses) UpdateExpense(id int, change string, amount string, description string) error {
 	if id < 1 || id-1 > len(*e) {
 		return fmt.Errorf("%d out of range.", id)
 	}
@@ -32,24 +35,23 @@ func (e *Expenses) UpdateExpense(id int, change string, value ...string) error {
 		if i == id-1 {
 			switch change {
 			case "description":
-				(*e)[i].description = value[0]
+				(*e)[i].Description = description
 			case "amount":
-				f, _ := strconv.ParseFloat(value[0], 64)
-				(*e)[i].amount = f
+				f, _ := strconv.ParseFloat(strings.Replace(amount, " ", "", -1), 64)
+				(*e)[i].Amount = f
 			case "both":
-				(*e)[i].description = value[0]
-				f, _ := strconv.ParseFloat(value[1], 64)
-				(*e)[i].amount = f
+				(*e)[i].Description = description
+				f, _ := strconv.ParseFloat(amount, 64)
+				(*e)[i].Amount = f
 			}
 		}
 
 	}
+	fmt.Println(*e)
 	return nil
 }
 
 func (e *Expenses) ListExpenses() (string, error) {
-	ex := expense{description: "Grad gown", amount: 42, createdAt: time.Now()}
-	*e = append(*e, ex)
 	if len(*e) == 0 {
 		return "", errors.New("List is empty.")
 	}
@@ -57,11 +59,10 @@ func (e *Expenses) ListExpenses() (string, error) {
 	var formatted string
 	var formattedTime string
 	formatted = "\nExpenses:\n----------------"
-	for _, value := range *e {
-		sum += value.amount
-		formattedTime = value.createdAt.Format("02 Jan")
-		formatted += fmt.Sprintf("\n%s:   £%.2f | %s\n------------------\n", value.description, value.amount, formattedTime)
-		formatted += fmt.Sprintf("Total Expenses: %.2f", sum)
+	for index, value := range *e {
+		sum += value.Amount
+		formattedTime = value.CreatedAt.Format("02 Jan")
+		formatted += fmt.Sprintf("\n%d - %s:   £%.2f | %s\n------------------\n", index+1, value.Description, value.Amount, formattedTime)
 	}
 	return formatted, nil
 }
@@ -72,11 +73,10 @@ func (e *Expenses) DisplaySummary() (string, error) {
 		return "", errors.New("No items in the list of expenses.")
 	}
 	for _, value := range *e {
-		sum += value.amount
+		sum += value.Amount
 	}
 	formatted := fmt.Sprintf("Your total expenses are: £%.2f", sum)
 	return formatted, nil
-
 }
 
 func (e *Expenses) DeleteExpense(id int) error {
@@ -86,4 +86,26 @@ func (e *Expenses) DeleteExpense(id int) error {
 	index := id - 1
 	*e = append((*e)[index+1:], (*e)[:index]...)
 	return nil
+}
+
+func (e *Expenses) SaveFile(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	csvWriter := csv.NewWriter(file)
+	for _, value := range *e {
+		amountStr := strconv.FormatFloat(value.Amount, 'f', 2, 64)
+		timeStr := value.CreatedAt.Format("02-01-06 15:04")
+		row := []string{value.Description, amountStr, timeStr}
+		err := csvWriter.Write(row)
+		if err != nil {
+			return err
+		}
+	}
+	csvWriter.Flush()
+	return nil
+
 }
--- main/main.go
+++ main/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"project.com/expense"
 )
 
 func main() {
 
-	exp := expense.Expenses{}
+	filename := "expenses.csv"
+	exp, err := LoadCSV(filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
 	add := flag.Bool("add", false, "Adds an expense to the list")
 	view := flag.Bool("view", false, "View all expenses")
@@ -25,12 +33,11 @@ func main() {
 
 	switch {
 	case *add:
-		fmt.Println("Testing")
-		fmt.Fprintln(os.Stdout, "Enter the name of the Expense (Simple names recommended): ")
+		fmt.Fprintln(os.Stdout, "Enter the name of the Expense (Simple names recommended):")
 		scanner.Scan()
 		title := scanner.Text()
 
-		fmt.Fprintln(os.Stdout, "Enter the amount without a £ sign: ")
+		fmt.Fprintln(os.Stdout, "Enter the amount without a £ sign:")
 		scanner.Scan()
 		amount, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
@@ -38,6 +45,11 @@ func main() {
 			os.Exit(1)
 		}
 		exp.AddExpense(title, amount)
+		if err := exp.SaveFile(filename); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
 	case *view:
 		output, err := exp.ListExpenses()
 		if err != nil {
@@ -48,43 +60,53 @@ func main() {
 		if err := exp.DeleteExpense(*delete); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
+		if err := exp.SaveFile(filename); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
 	case *update != 0:
 		var description string
 		var amount string
-		fmt.Fprintln(os.Stdout, "Enter what you would like to change (description, expense or both): ")
+		fmt.Fprintln(os.Stdout, "Enter what you would like to change (description, amount or both):")
 		scanner.Scan()
 		var change string = scanner.Text()
 		switch change {
 		case "description":
-			fmt.Fprintln(os.Stdout, "Enter the new description: ")
+			fmt.Fprintln(os.Stdout, "Enter the new description:")
 			scanner.Scan()
 
-			description = scanner.Text()
-		case "expense":
-			fmt.Fprintln(os.Stdout, "Enter the new expense amount: ")
+			description = strings.Trim(scanner.Text(), " ")
+		case "amount":
+			fmt.Fprintln(os.Stdout, "Enter the new expense amount:")
 			scanner.Scan()
 
-			amount = scanner.Text()
+			amount = strings.Trim(scanner.Text(), " ")
 
 		case "both":
-			fmt.Fprintln(os.Stdout, "Enter the new description: ")
+			fmt.Fprintln(os.Stdout, "Enter the new description:")
 			scanner.Scan()
 
-			description = scanner.Text()
+			description = strings.Trim(scanner.Text(), " ")
 			fmt.Fprintln(os.Stdout, "Enter the new expense amount: ")
 			scanner.Scan()
 
-			amount = scanner.Text()
+			amount = strings.Trim(scanner.Text(), " ")
 		default:
 			fmt.Fprintln(os.Stderr, "Specified change is not one of the options.")
 			os.Exit(1)
 
 		}
-		err := exp.UpdateExpense(*update, change, description, amount)
+		err := exp.UpdateExpense(*update, change, amount, description)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 
 		}
+		if err := exp.SaveFile(filename); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
 	case *summary:
 		formatted, err := exp.DisplaySummary()
 		if err != nil {
@@ -93,5 +115,29 @@ func main() {
 		}
 		fmt.Fprintln(os.Stdout, formatted)
 	}
+
+}
+
+func LoadCSV(filename string) (expense.Expenses, error) {
+	var exp expense.Expenses
+	file, err := os.Open(filename)
+	if err != nil {
+		return expense.Expenses{}, nil
+	}
+
+	records, err := csv.NewReader(file).ReadAll()
+
+	for _, record := range records {
+		desc := record[0]
+		amo, _ := strconv.ParseFloat(record[1], 64)
+		time, _ := time.Parse("02-01-06 15:04", record[2])
+		data := expense.Expense{
+			Description: desc,
+			Amount:      amo,
+			CreatedAt:   time,
+		}
+		exp = append(exp, data)
+	}
+	return exp, nil
 
 }
